c670f12196c0dfb2f927f33bddb80d2255d50aeb
isadoramsouza <isadora_msouza@hotmal.com>
2019-01-30 13:26:38 -0200

    adicionando FutureBuilder

--- conversor_moedas/lib/main.dart
+++ conversor_moedas/lib/main.dart
@@ -11,7 +11,11 @@ void main() async {
 
 
   runApp(MaterialApp(
-    home:Home()
+    home:Home(),
+    theme: ThemeData(
+      hintColor: Colors.amber,
+      primaryColor: Colors.white
+    ),
   ));
 }
 
@@ -27,6 +31,10 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  double dolar;
+  double euro;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -59,7 +67,29 @@ class _HomeState extends State<Home> {
                         textAlign: TextAlign.center,)
                   );
                 }else{
-                  return Container(color: Colors.green);
+                  dolar = snapshot.data["results"]["currencies"]["USD"]["buy"];
+                  euro = snapshot.data["results"]["currencies"]["EUR"]["buy"];
+
+                  return SingleChildScrollView(
+
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                        Icon(Icons.monetization_on, size: 150.0, color: Colors.amber,),
+                        TextField(
+                          decoration: InputDecoration(
+                            labelText: "Reais",
+                            labelStyle: TextStyle(color: Colors.amber),
+                            border: OutlineInputBorder(),
+                            prefixText: "R\$"
+                          ),
+                          style: TextStyle(
+                            color: Colors.amber, fontSize: 25.0
+                          ),
+                        ),
+                      ],
+                    )
+                  );
                 }
             }
           })
