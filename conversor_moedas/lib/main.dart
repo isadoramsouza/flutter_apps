7154f09fc5aa47828bbbd59de57cab905c0536d9
isadoramsouza <isadora_msouza@hotmal.com>
2019-01-30 08:39:15 -0200

    parte inicial app conversor de moedas

---
+++ conversor_moedas/lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+
+
+
+const request = "https://api.hgbrasil.com/finance?format=json&key=60df7606";
+
+void main() async {
+
+
+  runApp(MaterialApp(
+    home:Home()
+  ));
+}
+
+Future<Map> getData() async{
+  http.Response response = await http.get(request);
+  return json.decode(response.body);
+
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text("Conversor"),
+        backgroundColor: Colors.amber,
+        centerTitle: true,
+      ),
+    );
+  }
+}
