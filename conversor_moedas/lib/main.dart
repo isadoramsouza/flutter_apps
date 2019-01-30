1c5a999a1e4e8331656d5ec707e54f380df88a3c
isadoramsouza <isadora_msouza@hotmal.com>
2019-01-30 08:42:58 -0200

    commit inicial

--- conversor_moedas/lib/main.dart
+++ conversor_moedas/lib/main.dart
@@ -30,7 +30,7 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
+      backgroundColor: Colors.green,
       appBar: AppBar(
         title: Text("Conversor"),
         backgroundColor: Colors.amber,
